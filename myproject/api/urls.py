e67de69d7bd9a2cdd9dc6c4f0495420b88886be9
“pragyann7” <pragyanstha1@gmail.com>
2025-07-01 22:26:08 +0545

    Added: new urlpattern for Home page

--- myproject/api/urls.py
+++ myproject/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import RegisterView, ProtectedView, LogoutView
+from .views import RegisterView, ProtectedView, LogoutView, HomeView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
@@ -8,4 +8,6 @@ urlpatterns = [
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('protected/', ProtectedView.as_view(), name='protected'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('home/', HomeView.as_view(), name='home'),
+    # path('forgotpass/', ForgotpassView.as_view(), name='forgotpass'),
 ]
