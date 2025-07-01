6a280f6bbb281906188211af7c7cd74ab75dd8ad
“pragyann7” <pragyanstha1@gmail.com>
2025-07-01 22:44:31 +0545

    Added & modified: Route, forhetpass and login

--- frontend2/src/App.jsx
+++ frontend2/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SignIn from '../components/SignIn'
 import SignUp from '../components/SignUp'
 import Home from '../components/Home'
+import Forgetpass from '../components/forgetpass'
 import PrivateRoute from "../components/PrivateRoute";
 import './App.css'
 
@@ -11,7 +12,8 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/" element={<SignIn />} />
+        <Route path="/login" element={<SignIn />} />
+        {/* <Route path="/forgetpass" element={<Forgetpass />} /> */}
         <Route path="/home" element={<PrivateRoute><Home /></PrivateRoute>} />
       </Routes>
     </BrowserRouter>
