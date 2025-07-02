d5d6017d92ecacfca2bfc75192631ac0298363c0
“pragyann7” <pragyanstha1@gmail.com>
2025-07-02 16:40:18 +0545

    Added: route path for - userlist

--- frontend2/src/App.jsx
+++ frontend2/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SignIn from '../components/SignIn'
 import SignUp from '../components/SignUp'
 import Home from '../components/Home'
+import Userlist from '../components/UserList'
 import Forgetpass from '../components/forgetpass'
 import PrivateRoute from "../components/PrivateRoute";
 import './App.css'
@@ -12,7 +13,8 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/login" element={<SignIn />} />
+        <Route path="/" element={<SignIn />} />
+        <Route path="/userlist" element={<Userlist />} />
         {/* <Route path="/forgetpass" element={<Forgetpass />} /> */}
         <Route path="/home" element={<PrivateRoute><Home /></PrivateRoute>} />
       </Routes>
