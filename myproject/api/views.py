ac51b13f090bcd7535e8424010edf86d73466955
“pragyann7” <pragyanstha1@gmail.com>
2025-07-02 16:44:04 +0545

    Added: new User view

--- myproject/api/views.py
+++ myproject/api/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
-from .serializers import RegisterSerializer
+from .serializers import RegisterSerializer, UserSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth.models import User
 
 
 class RegisterView(APIView):
@@ -43,6 +44,17 @@ class HomeView(APIView):
     def get(self, request):
         return Response({"message": f"Welcome {request.user.username}!",
                          "username": request.user.username})
+
+
+class UserView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+
 
 
 # class ForgotpassView(APIView):
