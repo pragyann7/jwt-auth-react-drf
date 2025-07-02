4f1597889945d9fc730c29c5579a639838880602
“pragyann7” <pragyanstha1@gmail.com>
2025-07-02 16:43:28 +0545

    Added: new url for userlist

--- myproject/api/urls.py
+++ myproject/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import RegisterView, ProtectedView, LogoutView, HomeView
+from .views import RegisterView, ProtectedView, LogoutView, HomeView, UserView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
@@ -9,5 +9,6 @@ urlpatterns = [
     path('protected/', ProtectedView.as_view(), name='protected'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('home/', HomeView.as_view(), name='home'),
+    path('userlist/', UserView.as_view(), name='userlist'),
     # path('forgotpass/', ForgotpassView.as_view(), name='forgotpass'),
 ]
