14eaf71e73a6e78bc498cdef04bce0e370e7aa6f
“pragyann7” <pragyanstha1@gmail.com>
2025-07-03 22:19:10 +0545

    Added: new Product Serailizer

--- myproject/api/serializers.py
+++ myproject/api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 from django.contrib.auth.models import User
+from .models import Products
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
@@ -22,3 +23,17 @@ class UserSerializer(ModelSerializer):
     class Meta:
         model = User
         fields = ['id','username', 'email', 'date_joined']
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    image = serializers.ImageField()
+
+    class Meta:
+        model = Products
+        fields = '__all__'
+
+    def get_image(self, obj):
+        request = self.context.get('request')
+        if obj.image and request:
+            return request.build_absolute_uri(obj.image.url)
+        return None
