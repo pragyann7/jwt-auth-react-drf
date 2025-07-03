ca6cbb23161dc93cd9340c91421df75171f6b208
“pragyann7” <pragyanstha1@gmail.com>
2025-07-03 22:13:22 +0545

    Added: Product List(display products)

---
+++ frontend2/components/ProductList.jsx
@@ -0,0 +1,96 @@
+import { useState, useEffect } from "react"
+import axiosInstance from "../src/api/axios"
+import { useNavigate, Navigate } from "react-router-dom"
+import ErrorAuth from "../components/ErrorAuth"
+
+
+function ProductList() {
+
+    const [productList, setProductlist] = useState([]);
+    const [error, setError] = useState(null);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+                const res = await axiosInstance.get('products/');
+                setProductlist(res.data);
+            } catch (err) {
+                console.error("Failed to fetch products", err);
+                setError('Failed to fetch data or unauthorized');
+            }
+        };
+        fetchProducts();
+    }, []);
+
+    // if (error) return <ErrorAuth />;
+
+    const handleLogout = async () => {
+
+        const confirmed = window.confirm("Do you want to log out?");
+        if (!confirmed) return;
+
+        const refresh = localStorage.getItem('refresh');
+        const access = localStorage.getItem('access');
+        if (!refresh) {
+            localStorage.clear();
+            navigate('/');
+            return;
+        }
+
+        try {
+            const res = await axiosInstance.post('logout/', { refresh }, { headers: { Authorization: `Bearer ${access}` } });
+            console.log(res.data.message)
+            navigate('/');
+        } catch (error) {
+            console.log(res.data.error)
+        } finally {
+            localStorage.clear();
+        }
+    };
+
+    return (
+        <div>
+            <div className="relative flex justify-end top-10 right-10">
+                <button
+                    type="submit"
+                    onClick={handleLogout}
+                    className="flex justify-center cursor-pointer rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                    Log Out
+                </button>
+            </div>
+            <div className="max-w-7xl mx-auto px-4 py-8">
+                <h1 className="text-3xl font-bold mb-6 text-center">Products</h1>
+                <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+                    {productList.map((product) => (
+                        <div
+                            key={product.id}
+                            className="bg-white border border-gray-200 rounded-xl shadow-md p-4 hover:shadow-lg transition"
+                        >
+                            {/* <img
+                            src={product.image}
+                            alt={product.product_name}
+                            className="w-full h-40 object-cover rounded-md mb-4"
+                        /> */}
+                            <img
+                                src={product.image || 'https://via.placeholder.com/150'}
+                                alt={product.product_name}
+                                className="w-full h-40 object-cover rounded-md mb-4"
+                            />
+
+                            <h2 className="text-xl font-semibold">{product.product_name}</h2>
+                            <p className="text-gray-500">{product.category}</p>
+                            <p className="text-lg font-bold text-blue-600 mt-2">${product.price}</p>
+                            <button className="mt-4 w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition">
+                                View Details
+                            </button>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ProductList
