2598e80a314f30e3120387e790e20d8dbbe56016
“pragyann7” <pragyanstha1@gmail.com>
2025-07-03 12:06:13 +0545

    minor change

--- frontend2/components/ErrorAuth.jsx
+++ frontend2/components/ErrorAuth.jsx
@@ -3,7 +3,7 @@ function ErrorAuth() {
     return (
         <div>
             <div className="flex flex-col justify-center items-center h-screen">
-                <h1>Authentication Error</h1>
+                <h1>Authentication Error...</h1>
                 <p>go back to <a href="/"><span className="text-blue-600 hover:underline">Login</span></a> page.</p>
             </div>
         </div>
