fe019e2065bc5aeb69e0610c816153e14a610869
“pragyann7” <146330072+pragyann7@users.noreply.github.com>
2025-07-10 20:54:23 +0545

    imported Navbar component

--- frontend2/components/Home.jsx
+++ frontend2/components/Home.jsx
@@ -1,12 +1,12 @@
 import { useState, useEffect } from "react"
 import axiosInstance from "../src/api/axios"
-import { useNavigate, Navigate } from "react-router-dom"
 import ErrorAuth from "./ErrorAuth";
+import HeaderNav from "./HeaderNav";
 
 
 function Home() {
 
-    const navigate = useNavigate();
+
     const [error, setError] = useState(null);
     const [username, setUsername] = useState('');
 
@@ -27,43 +27,6 @@ function Home() {
 
     if (error) return <ErrorAuth />;
 
-
-    // Logout garna ko lagi
-    const handleLogout = async () => {
-
-        const confirmed = window.confirm("Do you want to log out?");
-        if (!confirmed) return;
-
-        const refresh = localStorage.getItem('refresh');
-        const access = localStorage.getItem('access');
-        if (!refresh) {
-            localStorage.clear();
-            navigate('/');
-            return;
-        }
-
-        try {
-            const res = await axiosInstance.post('logout/', { refresh }, { headers: { Authorization: `Bearer ${access}` } });
-            console.log(res.data.message)
-            navigate('/');
-        } catch (error) {
-            console.log(res.data.error)
-        } finally {
-            localStorage.clear();
-        }
-    };
-
-    const handleUserlist = () => {
-        navigate('/userlist');
-    }
-    const handleAddProduct = () => {
-        navigate('/addproduct');
-    }
-    const handleProductlist = () => {
-        navigate('/productlist');
-    }
-
-
     return (
         <><div><video
             autoPlay
@@ -74,36 +37,7 @@ function Home() {
             src="../public/justicewillserve0001-0190.mp4"
         />
         </div>
-            <div className="relative flex justify-around">
-                <button
-                    type="submit"
-                    onClick={handleUserlist}
-                    className="flex mt-4 cursor-pointer justify-center rounded-md px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs focus-visible:outline-2 focus-visible:outline-offset-2 "
-                >
-                    List Users
-                </button>
-                <button
-                    type="submit"
-                    onClick={handleAddProduct}
-                    className="flex mt-4 cursor-pointer justify-center rounded-md px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs focus-visible:outline-2 focus-visible:outline-offset-2"
-                >
-                    Add Product
-                </button>
-                <button
-                    type="submit"
-                    onClick={handleProductlist}
-                    className="flex mt-4 cursor-pointer justify-center rounded-md px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs focus-visible:outline-2 focus-visible:outline-offset-2"
-                >
-                    List Products
-                </button>
-                <button
-                    type="submit"
-                    onClick={handleLogout}
-                    className="flex mt-4 justify-center cursor-pointer rounded-md px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs focus-visible:outline-2 focus-visible:outline-offset-2"
-                >
-                    Log Out
-                </button>
-            </div>
+            <HeaderNav />
             {/* <div className="relative flex items-center gap-20 justify-center h-screen">
                 <h2 className="text-5xl font-bold mb-4">Welcome to AuctionHub</h2>
                 <h1 className="text-center">Hello {username}, Welcome to my Website</h1>
