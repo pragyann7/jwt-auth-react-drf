3036a449d68f408460ea37d80957902cfa301d4f
“pragyann7” <146330072+pragyann7@users.noreply.github.com>
2025-07-10 21:02:47 +0545

    Added: Navbar

--- frontend2/components/UserList.jsx
+++ frontend2/components/UserList.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react"
 import axiosInstance from "../src/api/axios"
 import { useNavigate, Navigate } from "react-router-dom"
 import ErrorAuth from "../components/ErrorAuth"
+import HeaderNav from "./HeaderNav"
 
 function UserList() {
 
@@ -26,43 +27,10 @@ function UserList() {
     // if (error) return <Navigate to="/" />;
 
 
-
-    const handleLogout = async () => {
-
-        const confirmed = window.confirm("Do you want to log out?");
-        if (!confirmed) return;
-
-        const refresh = localStorage.getItem('refresh');
-        const access = localStorage.getItem('access');
-        if (!refresh) {
-            localStorage.clear();
-            navigate('/');
-            return;
-        }
-
-        try {
-            const res = await axiosInstance.post('logout/', { refresh }, { headers: { Authorization: `Bearer ${access}` } });
-            console.log(res.data.message)
-            navigate('/');
-        } catch (error) {
-            console.log(res.data.error)
-        } finally {
-            localStorage.clear();
-        }
-    };
-
     return (
         <div>
-            <div className="relative flex justify-end top-10 right-10">
-                <button
-                    type="submit"
-                    onClick={handleLogout}
-                    className="flex justify-center cursor-pointer rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                    Log Out
-                </button>
-            </div>
-            <h2 className="text-4xl font-serif font-bold text-center">User List</h2>
+            <HeaderNav />
+            <h2 className="text-4xl mt-11 font-serif font-bold text-center">User List</h2>
             <div className="mt-4 w-full  flex justify-center items-center">
                 <div className="max-w-4xl w-full max-h-[570px] overflow-y-auto  border-black">
                     <table className="table-auto w-full max-w-4xl max-h-40 overflow-scroll border border-collapse border-black">
